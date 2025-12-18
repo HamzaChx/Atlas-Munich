5ba99ace0f7aac496beb9c15ab35ca520cf99329
HamzaChkx <chaoukihamza16@gmail.com>
2025-12-19 00:13:40 +0100

    fix: remove unused classes to fix build fail

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -3,5 +3,3 @@ export * from "./Stats";
 export * from "./Categories";
 export * from "./FeaturedGuides";
 export * from "./Events";
-export * from "./Testimonials";
-export * from "./ContributeCTA";
